24f5c80969c8638fea0859413d6b973f90a28eef
ayeshhaalii <ayeshhaalii00@gmail.com>
2025-11-23 11:42:12 +0000

    Update Ex3.py

--- Exercise 03 - Biography/Ex3.py
+++ Exercise 03 - Biography/Ex3.py
@@ -1,11 +1,11 @@
-#Create an empty dictionary
+# Create an empty dictionary
 person = {}
 
-#Get user input
+# Get user input
 person["name"] = input ("Enter your full name:")
 person ["hometown"] = input ("Enter your hometown:")
 
-#Trying to ensure age is entered as number
+# Trying to ensure age is entered as number
 while True: 
     try: 
         person["age"] = int(input("Enter your age:"))
@@ -13,5 +13,5 @@ while True:
     except ValueError:
         print("Please enter your age as a number (e.g.,20).")
 
-#Print the information
+# Print the information
 print(f"\nName: {person['name']}\nHometown: {person['hometown']}\nAge: {person['age']}")
